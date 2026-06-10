f2f6e2e7408aadf4b369e1a1e075ada465158ceb
Gounderpriyanka <gounderpriyanka4@gmail.com>
2026-06-10 21:29:59 +0530

    Add 7june.py with basic arithmetic operations and commented tasks

---
+++ 7june.py
@@ -0,0 +1,26 @@
+
+#Task1
+# a = int(input("Enter a number:"))
+# b = int(input("Enter a number:"))
+# print(a,end="")
+# print(b)
+
+#Task2
+
+# a = int(input("Enter the number:"))
+# b = int(input("Enter the second number:"))
+# c = float(a/b)
+# print(c)
+
+
+#Task3
+a = int(input("Enter a number:"))
+b = int(input("Enter a number:"))
+c = input("Enter a any of these operator (+, -, /, %, *) : ")
+c == "+" and  print(a+b)
+c == "-" and print(a-b)
+c == "/" and print(a/b)
+c == "%" and print(a%b)
+c == "*" and print(a*b)
+
+
