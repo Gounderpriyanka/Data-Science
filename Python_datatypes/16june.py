bcfab962d6b65849f5f10d2b0447647c0bf7fd15
Gounderpriyanka <gounderpriyanka4@gmail.com>
2026-06-17 15:51:31 +0530

    ..

--- Python_datatypes/16june.py
+++ Python_datatypes/16june.py
@@ -1,17 +1,62 @@
-total = int(input("Enter the total number:"))
-l1 = []
-oddsum = 0
-evensum = 0
-for i in range(1,total+1):
-    num = int(input("enter a number:"))
-    l1.append(num)
+# total = int(input("Enter the total number:"))
+# l1 = []
+# oddsum = 0
+# evensum = 0
+# for i in range(1,total+1):
+#     num = int(input("enter a number:"))
+#     l1.append(num)
     
-print("Sum",sum(l1))
-for i in l1:
-    if i%2==0:
-        evensum +=i
-    else:
-        oddsum +=i
-print("odd sum:",oddsum)
-print("Even sum:",evensum)
+# print("Sum",sum(l1))
+# for i in l1:
+#     if i%2==0:
+#         evensum +=i
+#     else:
+#         oddsum +=i
+# print("odd sum:",oddsum)
+# print("Even sum:",evensum)
         
+
+
+# palindrome number  121,1331
+
+# start = int(input("Enter a starting number:"))
+# end = int(input("Enter the end number:"))
+
+
+
+# for i in range(start,end+1): 
+#     temp = i
+#     reverse = 0
+#     for j in range(len(str(i))):
+#         r = temp%10
+#         reverse = (reverse*10) + r
+#         temp = temp//10
+#         if i == reverse:
+#             print(i,end=" ")
+
+num = int(input("Enter the value:"))
+while num>0:
+
+
+
+
+
+# to store a prime numbers in another list
+# num = int(input("enter the total number:"))
+# l1 = []
+# l2 = []
+
+# for i in range(num):
+#     s = int(input("Enter the number:"))
+#     l1.append(s)
+# for i in l1:
+#     c = 0
+#     for j in range(1,i+1):
+#         if i%j==0:
+#             c = c+1
+#     if c==2:
+#         l2.append(j)
+# print(l1)
+# print(l2)
+
+
