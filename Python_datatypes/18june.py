6858bf7a9744b4d179b6021d98706de09af6f59e
Gounderpriyanka <gounderpriyanka4@gmail.com>
2026-06-18 22:06:27 +0530

    ..

---
+++ Python_datatypes/18june.py
@@ -0,0 +1,12 @@
+# Task1
+# total =int(input("Enter the Total number:"))
+# l1 = []
+# for i in range (total):
+#     num = int(input("Enter the number:"))
+#     l1.append(num)
+# sorted_l1 = sorted(l1)
+# second_largest = sorted_l1[-2]
+# print("List:",l1)
+# print("The second Largest number is :",second_largest)
+
+#Task 2
