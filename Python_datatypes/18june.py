eccf2c4812af5cd3184c53dcb91e39cba756507c
Gounderpriyanka <gounderpriyanka4@gmail.com>
2026-06-19 12:19:05 +0530

    ...

--- Python_datatypes/18june.py
+++ Python_datatypes/18june.py
@@ -10,6 +10,25 @@
 # print("The second Largest number is :",second_largest)
 
 #Task 2
+# total =int(input("Enter the Total number:"))
+# l1 = []
+# l2 = []
+# for i in range (total):
+#     num = int(input("Enter the number:"))
+#     l1.append(num)
+
+# for i in l1:
+#     c = 0
+#     for j in range(1,i+1):
+#         if i%j==0:
+#             c = c+1
+#     if c == 2:
+#         l2.append(i)
+
+# print(l1)
+# print(l2)
+    
+# Task3
 total =int(input("Enter the Total number:"))
 l1 = []
 l2 = []
@@ -17,14 +36,14 @@ for i in range (total):
     num = int(input("Enter the number:"))
     l1.append(num)
 
-for i in l1:
-    c = 0
-    for j in range(1,i+1):
-        if i%j==0:
-            c = c+1
-    if c == 2:
-        l2.append(i)
+for i in range (num+1):
+    reverse = 0 
+    for j in l1:
+        temp = j   
+        r = i%10
+        reverse = reverse+r*10
+    if reverse == temp :
+        l2.append(j)
 
 print(l1)
-print(l2)
-    
+print(l2)       
