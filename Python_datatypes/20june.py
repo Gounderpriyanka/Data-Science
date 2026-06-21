6fe4fd0a1850cfa8dd9aef7ea21d78381b6a2e65
Gounderpriyanka <gounderpriyanka4@gmail.com>
2026-06-21 18:56:07 +0530

    ...

--- Python_datatypes/20june.py
+++ Python_datatypes/20june.py
@@ -1,7 +1,48 @@
-student = int(input("Enter the total student:"))
-d1 = {}
-for i in range (student):
-    name = input("Enter the name:")
-    marks = int(input("Enter the marks:"))
-    d1[name] = marks
-print(d1)
+# student = int(input("Enter the total student:"))
+# d1 = {}
+# for i in range (student):
+#     name = input("Enter the name:")
+#     marks = int(input("Enter the marks:"))
+#     d1[name] = marks
+# print(d1)
+
+# Task1
+num = int(input("Enter the number:"))
+for i in range(num+1):
+                                        
+     for j in range(i):
+        print("*",end="")
+
+     print("") 
+
+#task2
+num = int(input("Enter the number:"))
+for i in range(num,0,-1):
+    for j in range(i):
+        print("*",end="")
+    print("") 
+
+#task3
+num = int(input("Enter the number:"))
+for i in range(num,0,-1):
+    print(" "*(num-i),end="")
+    for j in range(i):
+        print("*",end="")
+    print("")
+
+#task4
+num = int(input("Enter the number:"))
+for i in range(1,num+1):
+    print(" "*(num-i),end="")
+    for j in range(i):
+    
+        print("*",end="")
+        # print(""*(num-i),end="") 
+    print("")    
+
+num = int(input("Enter the number:"))
+for i in range(num,1,-1):
+    for j in range(i):
+        print(" "*(num-1),"*",end="")
+    print("")         
+
