6a5a5de49c29eb733106bcb73b9c17e54b5a167f
Gounderpriyanka <gounderpriyanka4@gmail.com>
2026-07-02 22:32:55 +0530

    ...

--- Python_datatypes/Practice questions/dictionary.py
+++ Python_datatypes/Practice questions/dictionary.py
@@ -26,21 +26,76 @@ d1["ers"] = 67
 print(d1)'''
 
 # Q.5  Delete a key-value pair.
-d1 = {"abs":23,
+'''d1 = {"abs":23,
       "ers":45,
       "llm":32}
 d1.pop("ers")
 print(d1)
-
+'''
 # Q.6 Get all keys from a dictionary.
-d1 = {"abs":23,
+'''d1 = {"abs":23,
       "ers":45,
       "llm":32}
-print(d1.keys())
+print(d1.keys())'''
 
-#  Q.
+#  Q.7 Get all values from a dictionary.
+'''d1 = {"abs":23,
+      "ers":45,
+      "llm":32}
+print(d1.values())'''
+
+# Q.8 Get all key-value pairs.
+"""d1 = {"abs":23,
+      "ers":45,
+      "llm":32}
+print(d1)
+"""
+# Q.9 Check whether a key exists or not.
+'''d1 = {"abs":23,
+      "ers":45,
+      "llm":32}
+key = input("Enter a key to check:")
+if key in d1:
+    print("Yes")
+else:
+    print("no")'''
+
+# Q.10 Merge two dictionaries.
+'''d1 = {"abs":23,
+      "ers":45,
+      "llm":32}
+d2 = { "phy":45,
+       "math":78,
+       "chemis":63}
+print(d1.update(d2))
+print(d1)
+'''
+
+# Q.11 Find the key having the maximum value.
+'''d2 = { "phy":45,
+       "math":78,
+       "chemis":63}
+print(max(d2))'''
+
+# Q.12 Find the key having the minimum value.
+d2 = {"phy":45,
+      "math":78,
+      "chemistry":63}
+print(min(d2))
+
+
+
 # Q.13 Count frequency of characters in a string using a dictionary.
-
+'''s1 = "mississappi"
+d1 = {}
+for i in s1:
+    count = 0
+    for j in s1:
+        if i ==j:
+            count +=1
+    d1[i] = count
+print(d1)
+'''
 
 # Q.14 Create a dictionary from two lists.
 '''l1 = ["abc","cdv","gsd","dsd"]
@@ -59,3 +114,4 @@ for i in sorted(d1):
 print(sorted_dic)
 print(sorted_dic1)'''
 
+# Q.16 Write a program to find duplicate values in a dictionary.
