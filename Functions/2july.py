6bb364ec0388ad54fc2ffd89b159be8a0f2ca460
Gounderpriyanka <gounderpriyanka4@gmail.com>
2026-07-06 13:12:38 +0530

    ...

--- Functions/2july.py
+++ Functions/2july.py
@@ -1,3 +1,20 @@
-l1 = [19,65,57,39,152,639,121,44,90,190]
-l2 = []
-result = list[lambda y : y  ]
+
+# print in lambda
+'''a = lambda x: print("my name is:",x)
+a ("Priyanka")
+a ("sara")'''
+
+# 
+'''result = lambda a,b : print("a is big",a) if a>b else(print("b is big",b))
+result(23,5)'''
+
+#
+'''r = lambda a,b,c : print(min(a,b,c))
+r(1,6,89)
+r1 = lambda a,b,c : print(max(a,b,c))
+r1(56,23,45)
+r2 = lambda a,b,c : sorted([a,b,c])
+print(r2(45,89,63))'''
+
+#
+
