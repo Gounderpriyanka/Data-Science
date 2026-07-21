1cfede115d87536e973d340380fcc07b3370ad5c
Gounderpriyanka <gounderpriyanka4@gmail.com>
2026-07-21 15:56:41 +0530

    ......

---
+++ Class/21july.py
@@ -0,0 +1,35 @@
+class bank :
+    balance =25000 
+    pin  = 1234
+    def deposit(self):
+        
+        num = int(input("Enter the pin: "))
+        if self.pin == num:
+            amt = int(input("Enter the deposit amount: "))
+            self.balance  +=amt
+            print("deposited successfully")
+        else :
+            print("The pin is incorrect!")
+
+        
+    
+    def withdraw(self):
+        num = int(input("Enter the pin: "))
+        if self.pin == num:
+            amt = int(input("Enter the withdraw amount: "))
+            if self.balance -amt >=10000 :  # 30000 - 22000 >=10000 
+                self.balance -=amt
+                print("withdrawed successfully")
+            else :
+                print("min balance required 10000")
+        else:
+            print("Pin is incorrect!")
+
+
+    def display(self):
+        print("balance : ",self.balance)
+
+b = bank()
+b.deposit()
+b.withdraw()
+b.display()
