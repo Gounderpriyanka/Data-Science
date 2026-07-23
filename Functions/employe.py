3a2f1a53ac6213728a3431636ad3fa036a6e50d3
Gounderpriyanka <gounderpriyanka4@gmail.com>
2026-07-23 22:59:11 +0530

    Refactor employee management functions and enhance account creation logic

--- Functions/employe.py
+++ Functions/employe.py
@@ -1,9 +1,23 @@
-account = []
+"""
+bank app  : 
+
+1. create  account :
+    create  a  user name  : dishant 
+    create  a  password  : dish@1211D    ===> 1 uppercase , 1 lowercase , 1 digit , 1 special character
+
+2. login : 
+        enter user name  : dishant 
+        enter password  : dish@1211D
+            ====> print  login success   ====> ac 25000 
+            
+1. deposit  
+2. withdraw   : min balace 10000 maintain 
+3. check balance 
+4. exit 
+"""
+
+emp_account = []
 user = ""
-
-
-
-
 
 def create_password(password):
     
@@ -41,11 +55,11 @@ def create_account():
     password =input("enter password : ")
     
     if  create_password(password) :
-        account[username] = {
-            
+        emp_account.append( {
+            "username" : username,
             "password" : password,
-            
-        }
+            "balance" : 25000
+        })
 
         print("account created successfully")
     else :
@@ -57,11 +71,70 @@ def login():
     username =input("enter username : ")
     password =input("enter password : ")
     
-    if username in account :
-        if account[username]["password"] == password:
-            user = username
-            print("Login Success") 
+    for account in emp_account :
+        if account["username"] == username:
+            if account["password"] == password:
+                user = username
+                print("Login Success") 
         else: 
             print("password is not valid")
     else :
         print("username is not valid") 
+
+
+        
+def add_employee():
+    id =int(input("enter id : "))
+    name =input("enter name : ")
+    salary =int(input("enter salary : "))
+    age =int(input("enter age : "))
+    emp_account.append( { "id":id,
+                        "name":name,
+                        "salary":salary,
+                        "Age":age})
+    
+c
+
+def delete_employee():
+    id =int(input("enter id : "))
+    if  id in d1 :
+        del d1[id]
+    else :
+        print("id not found")
+        
+def display_employee():
+    # logic
+    id =int(input("enter id : "))
+    if  id in d1 :
+        print(d1[id])
+    else :
+        print("id not found")
+    
+# def main():
+#     while True:
+#         print("1. add employee")
+#         print("2. delete employee")
+#         print("3. update employee")
+#         print("4. display employee")
+#         print("5. exit")
+#         choice =int(input("enter choice : "))
+#         match choice :
+#             case 1 :
+#                 add_employee()
+#             case 2 :
+#                 delete_employee()
+#             case 3 :
+#                 update_employee()
+#             case 4 :
+#                 display_employee()
+#             case 5 :
+#                 break
+# main()
+    
+      
+# bank  app  () : 
+
+add_employee()
+add_employee()
+add_employee()
+print(emp_account)
